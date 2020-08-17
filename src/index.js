764e7620601a9b0e94624d2e54db746cfe4e750b
Kateryna Vyshnevetska <vyshnevetska.k@gmail.com>
2020-08-17 15:44:14 +0300

    Added HW12

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
 import './styles.css';
+import { default as fetchCountries} from './fetchCountries';
+import { debounce } from "../node_modules/debounce";
+
+import { error } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+
+import liTemplateByName from './template/li-names.hbs';
+import liTemplateByItem from './template/li-template.hbs';
+
+const refs = {
+  input: document.getElementById('js-input'),
+  list: document.getElementById('js-list'),
+}
+
+const drawList = data => {
+  const dataLength = data.length;
+  if(data.length > 10){
+    refs.list.innerHTML = '';
+    error({
+      text: 'Too many matches found. Please enter a more specific query',
+      hide: false,
+      sticker: false,
+      click: function(){
+        error.remove();
+      }
+    });
+  }
+  else if(data.length >= 2 && dataLength <= 10){
+    refs.list.innerHTML = '';
+    const item = liTemplateByName(data);
+    refs.list.insertAdjacentHTML('afterbegin',item);
+  }
+  else if(data.length === 1){
+    refs.list.innerHTML = '';
+    const item = liTemplateByItem(data);
+    refs.list.insertAdjacentHTML('afterbegin',item);
+  }
+}
+
+const getDataWithDebounce = debounce(function(i) {
+  const allData = fetchCountries(i);
+  allData.then(data => drawList(data));
+}, 500);
+
+
+const checkInput = (ev) => {
+  const inputLength = ev.target.value.length;
+  if(inputLength){
+    getDataWithDebounce(ev.target.value);
+  }
+}
+
+refs.input.addEventListener('input', checkInput)
+
